3c211ca0ffe5ab9fd681bc25abda57ef690db95d
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-06-17 20:01:11 -0300

    added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="">
+      <h1 className="">
+        Desarrollado por
+        <a href="https://enriquez-jose.vercel.app/" className="cursor-pointer">
+          Jose Enriquez
+        </a>
+      </h1>
+    </footer>
+  );
+}
