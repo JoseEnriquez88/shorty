c9fd48507be7f9005948373fb63b2fbd16ad666c
JoseEnriquez88 <eenriquez.jose@gmail.com>
2025-06-17 20:18:22 -0300

    updated route

--- src/app/api/[slug]/route.ts
+++ src/app/api/[slug]/route.ts
@@ -1,13 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
 import { redis } from "@/lib/redis";
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { slug: string } }
 ) {
   const { slug } = params;
+  if (!slug) return new NextResponse("Slug requerido", { status: 400 });
   const originalUrl = await redis.get<string>(`slug:${slug}`);
-  if (!originalUrl) {
-    return new Response("Not found", { status: 404 });
-  }
-  return Response.redirect(originalUrl, 301);
+  if (!originalUrl)
+    return new NextResponse("Slug no encontrado", { status: 404 });
+  return NextResponse.redirect(originalUrl, 301);
 }
